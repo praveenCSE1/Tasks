5befeedce9d958b3424f70d385d8dcff8f3c6d17
PRAVEEN <2k20cse107@kiot.ac.in>
2024-01-25 11:38:41 +0530

    Included bcrypt to store the hashed password

--- controllers/regControllers.js
+++ controllers/regControllers.js
@@ -1,13 +1,21 @@
 const express = require('express')
 const mongoose = require('../model/db.js')
 const user = require('../model/schema.js')
+const bcrypt = require('bcrypt')
 
 
-const signup = (req,res)=>{
+const signup = async (req,res)=>{
     
     const{email,password}=req.body;
-    console.log(req.body);
-    const n_user = new user({email,password});
+
+    const existingUser = await user.findOne({ email });
+    if (existingUser) {
+            return res.status(400).json({ message: 'Email already exists' });
+        }
+    //Hashing the password with 10 saltrounds before storing the data
+    const hpassword = await bcrypt.hash(password,10)
+    console.log(hpassword);
+    const n_user = new user({email,password:hpassword});
     n_user.save();
      res.status(200).json({message:'user saved successfully'})  
 }
@@ -16,16 +24,27 @@ const login = async (req, res) => {
     try {
       const { email, password } = req.body;
       const db = mongoose.connection; 
-      const user = await db.collection('registers').findOne({ email, password });
+      const user = await db.collection('registers').findOne({email});
+
+      //check whether the user with this email exist or not.
       if (user) {
+
+        const u_password = await bcrypt.compare(password, user.password);
+
+        //check whether the email and password matches
+        if(u_password){
         console.log("login Successfull");
         res.status(200).json({message:'login successfull'})
-      } else {     
-        return res.status(401).json({ message: 'Invalid credentials' });
+        }
+        else{
+          return res.status(401).json({ message: 'Invalid password' });
+        }
+      } else {
+        return res.status(401).json({ message: 'Email not found' });
       }
     } catch (error) {
       console.error(error);
-      res.status(500).json({ message: 'Internal Server Error' });
+      res.status(500).json({ message: 'Internal Server Error'});
     }
   }
 
