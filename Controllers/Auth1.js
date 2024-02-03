286527d10f3e23a910d66369fd1657b8cd88e186
PRAVEEN <2k20cse107@kiot.ac.in>
2024-02-03 11:46:53 +0530

    Included google Authentican and stored the id in the session

--- Controllers/Auth1.js
+++ Controllers/Auth1.js
@@ -1,34 +1,28 @@
 const Guser = require('./db')
 const express = require('express')
 const passport = require('passport')
+
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 require('dotenv').config();
+const app = express()
 
-const session = require('express-session');
-const app = express();
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-app.use(session({
-    secret: 'secretkey', 
-    resave: false,
-    saveUninitialized: false
-  }));
 
 
 passport.serializeUser((user, done) => {
-
   done(null, user.id);
 });
 
 passport.deserializeUser((id, done) => {
-  console.log()
-  Guser.findById(id).then((obj)=>{
-    console.log(obj)
-    done(null,obj)
-  })
+  
+  Guser.findById(id)
+    .then(user => {
+      done(null, user);
+    })
+    .catch(err => {
+      done(err);
+    });
 });
+
 
 passport.use(new GoogleStrategy({
     
@@ -36,34 +30,31 @@ passport.use(new GoogleStrategy({
     clientSecret: process.env.CLIENT_SECRET,
     callbackURL: "http://localhost:5000/auth/google/callback"
   },
-  async function(accessToken, refreshToken, profile, cb) {
+  async function(accessToken, refreshToken, profile, done) {
     
    Guser.findOne({email:profile.emails[0].value})
     .then(user=>{
       
       if (user) {
-       
 
-        cb(null, user);
-      } else {
-        
+        done(null, user);
+       
+      } else {       
          const newuser = new Guser({
           name: profile.displayName,
           email:  profile.emails[0].value,
           googleId: profile.id
         });
-         newuser.save();
+        newuser.save().then(nuser=>{
+        return done(null,nuser)
+
+       })
         
     }
   })
-    .then(newuser=>{
-      
-       cb(null, newuser);
-        
-      })
-      .catch(error => {
+  .catch(error => {
        
-        return cb(error);
+         done(error,false);
       });
 
     }    
--- app.js
+++ app.js
@@ -1,17 +1,45 @@
 const express = require('express')
 const app = express();
 const passport=require('./Controllers/Auth1')
+const session = require('express-session');
+
+app.use(session({
+  secret: process.env.GOOGLE_CLIENT_ID, 
+  resave: true,
+  saveUninitialized: true,
+  name: 'user', 
+  cookie: {  
+    maxAge: 24 * 60 * 60 * 1000, 
+    secure: false, 
+    httpOnly: true, 
+  }
+}));
+
+
+ app.use(passport.initialize());
+
+ //responsible for deserializing the serialized(in passport) data when requested
+ app.use(passport.session());
 
 
 
 app.get('/auth/google',
   passport.authenticate('google', { scope: ['profile', 'email'] }));
-  app.get('/auth/google/callback', 
-  passport.authenticate('google', { failureRedirect: '/login' }),
+
+  
+app.get('/auth/google/callback', 
+  passport.authenticate('google'),
   function(req, res) {
-    console.log(req.body);
     res.redirect('/success');
   });
+
+  app.get('/success',(req,res)=>{
+    res.send(req.user)
+})
+
+app.get('/ss',(req,res)=>{
+  res.send(req.session)
+})
 
 
 
